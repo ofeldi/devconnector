a4b5de9793709872bd10d01277bc89dad01485e1
Oren Feldman <Oren.Feldman@telit.com>
2020-04-19 13:43:38 +0300

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://devconnector:vmsH9zVUCRUDSfnT@vacations-n7go5.mongodb.net/test?retryWrites=true&w=majority'
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+//@route GET api/posts/profile
+//@desc Tests profile route
+//@access Public
+
+router.get('/test', (req,res) => res.json({
+    msg:"profile works"
+}));
+
+module.exports = router;
